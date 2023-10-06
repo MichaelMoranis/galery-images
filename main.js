d38b0fd2c28a4f0a2ed64bec276e2aaf8a51da99
MichaelMoranis <michaelmoranesul@gmail.com>
2023-10-06 17:45:28 -0300

    configurar botao que de dark para light e vs versa

--- main.js
+++ main.js
@@ -23,8 +23,17 @@ for(let i = 0; i < arrayImages.length; i++) {
     } )
 }
 
-function setImage(src) {
-
-}
+btn.addEventListener("click", () => {
+    const btnClass = btn.getAttribute("class");
+    if(btnClass === "dark") {
+       btnClass.setAttribute("light");
+       btn.textContent = "lighten";
+       overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btnClass.setAttribute("dark");
+        btn.textContent = "Dark";
+        overlay.style.backgroundColor = "light";
+    }
+})
 
 /* Wiring up the Darken/Lighten button */
