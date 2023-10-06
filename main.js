e2f24f89b5c2fea842db2f4154b78a4fc2869e32
MichaelMoranis <michaelmoranesul@gmail.com>
2023-10-06 17:19:24 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+let arrayImages = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+for(let i = 0; i < arrayImages.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `./images/${arrayImages[i]}`);
+    newImage.setAttribute('alt', `./images/${arrayImages[i]}`);
+    thumbBar.appendChild(newImage);
+}
+
+/* Wiring up the Darken/Lighten button */
